debb4318163972fc63dddfd26723f68e53de5333
Yiyao Li <1256495914@qq.com>
2023-10-18 20:18:35 +0800

    Add chat with gpt api in flask

--- prediction-model/app.py
+++ prediction-model/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from flask import Flask, render_template, request, session, jsonify
+import openai
 from DataFunction import get_predict_para, get_model, predict_rate
 from flask_cors import CORS
 import secrets
 import string
+from CallGptAPI import *
+from utils import *
 
 app = Flask(__name__)
 CORS(app)
+
+# set api key
+api_key = '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'
+openai.api_key = api_key
+
+# connect to mysql
+cursor = connect_database()
 
 
 def generate_secret_key(length=24):
@@ -18,7 +28,7 @@ app.secret_key = generate_secret_key()
 
 
 @app.route('/form', methods=['GET', 'POST'])
-def hello_world():  # put application's code here
+def get_form_data():  # put application's code here
     if request.method == 'GET':
         return render_template('form.html')
     elif request.method == 'POST':
@@ -33,15 +43,27 @@ def hello_world():  # put application's code here
 
         response = {'message': rate}
         return jsonify(response)
-        # rate = PredictRate(res)
-        # session['rate'] = rate
-        # return redirect(url_for('show_rate'))
 
 
 @app.route('/rate')
-def show_rate():
+def get_rate():
     Rate = session.get('rate', None)
     return render_template('rate.html', Rate=Rate)
+
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    print('chat triggered')
+    print(request.json)
+    question = request.json['question']
+    user_id = request.json['user_id']
+    session_id = request.json['session_id']
+    context_load = os.path.exists('./storage/' + user_id)
+    print('context load? ', context_load)
+    print('parameters: ', question, user_id, session_id, )
+    # response = chat_with_gpt(question, user_id, session_id, cursor, context_load, True)
+
+    # return response
 
 
 if __name__ == '__main__':
